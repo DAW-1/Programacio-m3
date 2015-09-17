0e53ab8e1ad6cbd0b5096dda3c44837e7132eb97
spartan195 <xavi_ss_tgn@hotmail.com>
2015-09-17 21:00:20 +0800

    Println
    prova

---
+++ src/println/Println.java
@@ -0,0 +1,16 @@
+package println;
+
+/**
+ *
+ * @author xavi_banus_
+ */
+public class Println {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+    
+}
